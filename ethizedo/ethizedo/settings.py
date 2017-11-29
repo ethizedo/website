836ea464a87ce1b0e7592b2b85763f9963c9c3fc
ethizedo <mariondelalande@MacBook-Air.local>
2017-11-29 20:56:24 +0100

    static changed

--- ethizedo/ethizedo/settings.py
+++ ethizedo/ethizedo/settings.py
@@ -129,8 +129,5 @@ SITE_ID=1
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
